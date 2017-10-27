78543b78beae24101fc5460c705c9c8d74c020a4
Unknown <bryan@kerruish.ie>
2017-10-27 18:05:22 +0100

    tidy routes

--- api/routes/apiRoutes.js
+++ api/routes/apiRoutes.js
@@ -1,30 +1,27 @@
 'use strict'
 
-module.exports = function(app) {
-    var api = require('../controllers/apiController');
+module.exports = function (app) {
+  var api = require('../controllers/apiController');
 
-    // API Routes
-      app.get('/api/takePicture', function (req, res, next) {
-        api.take_a_picture()
-        next()
-      }, function (req, res) {
-        res.send('Take Picture')
-      })
+  // API Routes
+  app.get('/api/takePicture', function (req, res, next) {
+    api.take_a_picture()
+    next()
+  }, function (req, res) {
+    res.send('Take Picture')
+  })
 
-      app.get('/api/takePictureHourly', function (req, res, next) {
-        api.take_a_picture_hourly()
-        next()
-      }, function (req, res) {
-        res.send('Take Picture Hourly')
-      })
+  app.get('/api/takePictureHourly', function (req, res, next) {
+    api.take_a_picture_hourly()
+    next()
+  }, function (req, res) {
+    res.send('Take Picture Hourly')
+  })
 
-      app.get('/api/takePictureEveryX', function (req, res, next) {
-        api.take_a_picture_every_x()
-        next()
-      }, function (req, res) {
-        res.send('Take Picture Every X')
-      })
-
-
-
-    };
+  app.get('/api/takePictureEveryX', function (req, res, next) {
+    api.take_a_picture_every_x()
+    next()
+  }, function (req, res) {
+    res.send('Take Picture Every X')
+  })
+};
