0a22cc70afba429b93287ee9fc795c12168d9931
Unknown <bryan@kerruish.ie>
2017-10-27 14:30:40 +0100

    most simple take photo

--- middleware.js
+++ middleware.js
@@ -5,6 +5,27 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+
+var childProcess = require('child_process'),
+takePicture;
+
+takePicture = childProcess.exec('python take-picture.py', function (error, stdout, stderr) {
+if (error) {
+console.log(error.stack);
+console.log('Error code: '+error.code);
+console.log('Signal received: '+error.signal);
+}
+console.log('Child Process STDOUT: '+stdout);
+console.log('Child Process STDERR: '+stderr);
+});
+
+takePicture.on('exit', function (code) {
+console.log('Child process exited with exit code '+code);
+});
+
+
+
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Photo taken!')
 })
