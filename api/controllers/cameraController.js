4ffb8d39955bc0231fa99f72a89f1e11b6798c7c
kerruish <bryan@kerruish.ie>
2017-11-04 15:45:42 +0000

    change upload naming

--- api/controllers/cameraController.js
+++ api/controllers/cameraController.js
@@ -1,22 +1,34 @@
 'use strict';
 var firebase = require('../../api/controllers/firebaseController');
+var fs = require('fs');
+var moment = require('moment');
 
 exports.take_upload_picture = function (req, res) {
 
-    const { spawn } = require('child_process');
-    const captureImage = spawn('raspistill', ['-w', '640', '-h', '480', '-vf', '-hf', '-o', 'imagesCaptured/capture.jpg']);
-    
-    captureImage.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
+  const {
+    spawn
+  } = require('child_process');
+  const captureImage = spawn('raspistill', ['-w', '640', '-h', '480', '-vf', '-hf', '-o', 'imagesCaptured/capture.jpg']);
+
+  captureImage.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  captureImage.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+  });
+
+  captureImage.on('close', (code) => {
+    console.log(`Capture image exited with code: ${code}`)
+
+    var fileName = 'imagesCaptured/capture-' + moment().format() + '.jpg';
+    fs.rename('imagesCaptured/capture.jpg', fileName, function (err) {
+      if (err) console.log('File Rename Error: ' + err);
     });
     
-    captureImage.stderr.on('data', (data) => {
-      console.log(`stderr: ${data}`);
-    });
-    
-    captureImage.on('close', (code) => {
-      console.log(`Capture image exited with code: ${code}`)
-      firebase.upload_file('imagesCaptured/capture.jpg')
-    });
+    firebase.upload_file(fileName, function (err) {
+      if (err) console.log('Firebase Upload Error: ' + err);
+    })
+  });
 
 };
